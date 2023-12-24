757194a8017248b9315da02a21ae5488f3f8fca1
oncet <camilorivera86@gmail.com>
2023-12-24 08:10:51 -0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+const App = () => {
+  return (
+    <div className="border">
+      <div className="border border-red-500 text-9xl font-bold">あ</div>
+    </div>
+  );
+};
+
+export default App;
