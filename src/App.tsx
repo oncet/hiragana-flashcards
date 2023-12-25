5b0cbd243d7784f2be38c1601e2fe3870ab5fa43
oncet <camilorivera86@gmail.com>
2023-12-25 11:45:56 -0300

    add characters

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,298 @@
 import { useEffect, useState } from "react";
 import "./App.css";
 
+const syllables = [
+  {
+    kana: "あ",
+    romaji: "a",
+  },
+  {
+    kana: "い",
+    romaji: "i",
+  },
+  {
+    kana: "う",
+    romaji: "u",
+  },
+  {
+    kana: "え",
+    romaji: "e",
+  },
+  {
+    kana: "お",
+    romaji: "o",
+  },
+  {
+    kana: "か",
+    romaji: "ka",
+  },
+  {
+    kana: "き",
+    romaji: "ki",
+  },
+  {
+    kana: "く",
+    romaji: "ku",
+  },
+  {
+    kana: "け",
+    romaji: "ke",
+  },
+  {
+    kana: "こ",
+    romaji: "ko",
+  },
+  {
+    kana: "さ",
+    romaji: "sa",
+  },
+  {
+    kana: "し",
+    romaji: "shi",
+  },
+  {
+    kana: "す",
+    romaji: "su",
+  },
+  {
+    kana: "せ",
+    romaji: "se",
+  },
+  {
+    kana: "そ",
+    romaji: "so",
+  },
+  {
+    kana: "た",
+    romaji: "ta",
+  },
+  {
+    kana: "ち",
+    romaji: "chi",
+  },
+  {
+    kana: "つ",
+    romaji: "tsu",
+  },
+  {
+    kana: "て",
+    romaji: "te",
+  },
+  {
+    kana: "と",
+    romaji: "to",
+  },
+  {
+    kana: "な",
+    romaji: "na",
+  },
+  {
+    kana: "に",
+    romaji: "ni",
+  },
+  {
+    kana: "ぬ",
+    romaji: "nu",
+  },
+  {
+    kana: "ね",
+    romaji: "ne",
+  },
+  {
+    kana: "の",
+    romaji: "no",
+  },
+  {
+    kana: "は",
+    romaji: "ha",
+  },
+  {
+    kana: "ひ",
+    romaji: "hi",
+  },
+  {
+    kana: "ふ",
+    romaji: "fu",
+  },
+  {
+    kana: "へ",
+    romaji: "he",
+  },
+  {
+    kana: "ほ",
+    romaji: "ho",
+  },
+  {
+    kana: "ま",
+    romaji: "ma",
+  },
+  {
+    kana: "み",
+    romaji: "mi",
+  },
+  {
+    kana: "む",
+    romaji: "mu",
+  },
+  {
+    kana: "め",
+    romaji: "me",
+  },
+  {
+    kana: "も",
+    romaji: "mo",
+  },
+  {
+    kana: "や",
+    romaji: "ya",
+  },
+  {
+    kana: "ゆ",
+    romaji: "yu",
+  },
+  {
+    kana: "よ",
+    romaji: "yo",
+  },
+  {
+    kana: "ら",
+    romaji: "ra",
+  },
+  {
+    kana: "り",
+    romaji: "ri",
+  },
+  {
+    kana: "る",
+    romaji: "ru",
+  },
+  {
+    kana: "れ",
+    romaji: "re",
+  },
+  {
+    kana: "ろ",
+    romaji: "ro",
+  },
+  {
+    kana: "わ",
+    romaji: "wa",
+  },
+  {
+    kana: "を",
+    romaji: "wo",
+  },
+  {
+    kana: "ん",
+    romaji: "n",
+  },
+  {
+    kana: "が",
+    romaji: "ga",
+  },
+  {
+    kana: "ぎ",
+    romaji: "gi",
+  },
+  {
+    kana: "ぐ",
+    romaji: "gu",
+  },
+  {
+    kana: "げ",
+    romaji: "ge",
+  },
+  {
+    kana: "ご",
+    romaji: "go",
+  },
+  {
+    kana: "ざ",
+    romaji: "za",
+  },
+  {
+    kana: "じ",
+    romaji: "ji",
+  },
+  {
+    kana: "ず",
+    romaji: "zu",
+  },
+  {
+    kana: "ぜ",
+    romaji: "ze",
+  },
+  {
+    kana: "ぞ",
+    romaji: "zo",
+  },
+  {
+    kana: "だ",
+    romaji: "da",
+  },
+  {
+    kana: "ぢ",
+    romaji: "ji",
+  },
+  {
+    kana: "づ",
+    romaji: "zu",
+  },
+  {
+    kana: "で",
+    romaji: "de",
+  },
+  {
+    kana: "ど",
+    romaji: "do",
+  },
+  {
+    kana: "ば",
+    romaji: "ba",
+  },
+  {
+    kana: "び",
+    romaji: "bi",
+  },
+  {
+    kana: "ぶ",
+    romaji: "bu",
+  },
+  {
+    kana: "べ",
+    romaji: "be",
+  },
+  {
+    kana: "ぼ",
+    romaji: "bo",
+  },
+  {
+    kana: "ぱ",
+    romaji: "pa",
+  },
+  {
+    kana: "ぴ",
+    romaji: "pi",
+  },
+  {
+    kana: "ぷ",
+    romaji: "pu",
+  },
+  {
+    kana: "ぺ",
+    romaji: "pe",
+  },
+  {
+    kana: "ぽ",
+    romaji: "po",
+  },
+];
+
 const App = () => {
   const [theme, setTheme] = useState<"light" | "dark">("dark");
+  const [currentPosition, setCurrentPosition] = useState(0);
+
+  const currentSyllable = syllables[currentPosition];
 
   useEffect(() => {
     const htmlElement = document.querySelector("html");
@@ -21,7 +311,7 @@ const App = () => {
   return (
     <div className="flex min-h-svh items-center justify-center">
       <div className="flex h-96 max-w-72 flex-grow items-center justify-center rounded-xl text-9xl font-bold dark:bg-slate-800 dark:text-slate-300">
-        <div>あ</div>
+        <div>{currentSyllable.kana}</div>
       </div>
     </div>
   );
