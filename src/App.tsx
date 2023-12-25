9d027247d6bdde0fc83a1bd0d8784da5166180df
oncet <camilorivera86@gmail.com>
2023-12-25 10:10:08 -0300

    add dark mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 
 const App = () => {
+  const [theme, setTheme] = useState<"light" | "dark">("dark");
+
+  useEffect(() => {
+    const htmlElement = document.querySelector("html");
+
+    if (htmlElement) {
+      htmlElement.classList.add(theme);
+    }
+  }, []);
+
   return (
-    <div className="flex min-h-svh items-center justify-center">
-      <div className="flex h-96 max-w-72 flex-grow items-center justify-center border border-red-500 text-9xl font-bold">
+    <div className="dark flex min-h-svh items-center justify-center dark:bg-slate-600">
+      <div className="flex h-96 max-w-72 flex-grow items-center justify-center text-9xl font-bold dark:bg-slate-800">
         <div>あ</div>
       </div>
     </div>
