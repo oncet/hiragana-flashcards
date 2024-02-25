b5b502c5843956245af8d7efa7c84c4fbde5fe30
oncet <camilorivera86@gmail.com>
2024-02-25 01:16:21 -0300

    randon shuffle

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -4,6 +4,28 @@ import { Flashcard } from "./Flashcard";
 import { RightDropzone } from "./RightDropzone";
 import { WrongDropzone } from "./WrongDropzone";
 
+function shuffle(array) {
+  let currentIndex = array.length,
+    randomIndex;
+
+  // While there remain elements to shuffle.
+  while (currentIndex > 0) {
+    // Pick a remaining element.
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    // And swap it with the current element.
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex],
+      array[currentIndex],
+    ];
+  }
+
+  return array;
+}
+
+const shuffledSyllables = shuffle(syllables);
+
 const Main = () => {
   const [currentPosition, setCurrentPosition] = useState(0);
   const [rejectedSyllables, setRejectedSyllables] = useState<number[]>([]);
