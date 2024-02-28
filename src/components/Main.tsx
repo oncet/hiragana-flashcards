b1587547c74c992352b37b21e020fef713f19e4f
oncet <camilorivera86@gmail.com>
2024-02-27 22:03:09 -0300

    responsive view wip

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -39,41 +39,98 @@ const Main = () => {
     : syllables[currentPosition];
 
   return (
-    <div className="flex min-h-svh items-center justify-center gap-6">
-      <WrongDropzone
-        className="hidden md:flex"
-        onClick={() => {
-          if (!isWaiting || isLast) return;
+    <div className="flex h-svh flex-col justify-center">
+      <div className="mx-auto flex basis-[33%] items-center justify-between md:hidden">
+        <div className="flex flex-col items-center px-8 py-4">
+          <svg
+            viewBox="0 0 100 100"
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-[50px] w-[50px] text-red-500"
+          >
+            <line
+              x1="10"
+              y1="10"
+              x2="90"
+              y2="90"
+              stroke="currentColor"
+              strokeWidth="8"
+              strokeLinecap="round"
+            />
+            <line
+              x1="10"
+              y1="90"
+              x2="90"
+              y2="10"
+              stroke="currentColor"
+              strokeWidth="8"
+              strokeLinecap="round"
+            />
+          </svg>
+          <div
+            className={`flex items-center text-5xl font-thin transition [font-variant-numeric:tabular-nums] dark:text-red-400`}
+          >
+            0
+          </div>
+        </div>
+        <div className="flex flex-col items-center px-8 py-4">
+          <svg
+            viewBox="0 0 100 100"
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-[50px] w-[50px] text-green-500"
+          >
+            <circle
+              cx="50"
+              cy="50"
+              r="40"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="8"
+            />
+          </svg>
+          <div
+            className={`flex items-center text-5xl font-thin transition [font-variant-numeric:tabular-nums] dark:text-green-400`}
+          >
+            0
+          </div>
+        </div>
+      </div>
+      <div className="flex items-start justify-center gap-6 md:items-center">
+        <WrongDropzone
+          className="hidden md:flex"
+          onClick={() => {
+            if (!isWaiting || isLast) return;
 
-          setRejectedSyllables([...rejectedSyllables, currentPosition]);
-          setCurrentPosition(currentPosition + 1);
-        }}
-        rejectedSyllables={rejectedSyllables}
-      />
-      <Flashcard
-        currentSyllable={currentSyllable}
-        isRejected={rejectedSyllables.includes(currentPosition)}
-        onFlip={(isRomaji) => {
-          if (isRomaji) {
-            setIsWaiting(true);
+            setRejectedSyllables([...rejectedSyllables, currentPosition]);
+            setCurrentPosition(currentPosition + 1);
+          }}
+          rejectedSyllables={rejectedSyllables}
+        />
+        <Flashcard
+          currentSyllable={currentSyllable}
+          isRejected={rejectedSyllables.includes(currentPosition)}
+          onFlip={(isRomaji) => {
+            if (isRomaji) {
+              setIsWaiting(true);
 
-            return;
-          }
+              return;
+            }
 
-          setIsWaiting(false);
-        }}
-        isLast={isLast}
-      />
-      <RightDropzone
-        className="hidden md:flex"
-        onClick={() => {
-          if (!isWaiting || isLast) return;
+            setIsWaiting(false);
+          }}
+          isLast={isLast}
+        />
+        <RightDropzone
+          className="hidden md:flex"
+          onClick={() => {
+            if (!isWaiting || isLast) return;
 
-          setApprovedSyllables([...approvedSyllables, currentPosition]);
-          setCurrentPosition(currentPosition + 1);
-        }}
-        acceptedSyllables={approvedSyllables}
-      />
+            setApprovedSyllables([...approvedSyllables, currentPosition]);
+            setCurrentPosition(currentPosition + 1);
+          }}
+          acceptedSyllables={approvedSyllables}
+        />
+      </div>
+      <div className="basis-[33%] md:hidden" />
     </div>
   );
 };
