2fb74f6a95c11a1acbe5cacc6597b1665823fb50
oncet <camilorivera86@gmail.com>
2024-04-21 01:07:10 -0300

    add transition

--- src/components/AcceptButton.tsx
+++ src/components/AcceptButton.tsx
@@ -1,9 +1,20 @@
-import { useState } from "react";
+import { useState, useEffect, useRef } from "react";
+import { motion } from "framer-motion";
 
 import Circle from "./Circle";
 
 const AcceptButton = () => {
   const [count, setCount] = useState(4);
+  const [animate, setAnimate] = useState(false);
+
+  useEffect(() => {
+    if (animate) {
+      window.setTimeout(() => {
+        setAnimate(false);
+        console.log("timeout finished");
+      }, 200);
+    }
+  }, [animate]);
 
   return (
     <div className="relative">
@@ -11,13 +22,18 @@ const AcceptButton = () => {
         className="flex h-[110px] w-[110px] items-center justify-center rounded-full border-2 border-green-900"
         onClick={() => {
           setCount(count + 1);
+          setAnimate(true);
         }}
       >
         <Circle size="sm" />
       </button>
-      <div className="absolute bottom-0 right-0 flex h-10 w-10 items-center justify-center rounded-full border border-green-900 bg-slate-950 text-xl text-green-400 [font-variant-numeric:tabular-nums]">
+      <motion.div
+        animate={{ scale: animate ? 1.3 : 1 }}
+        transition={{ duration: 0.2 }}
+        className="absolute bottom-0 right-0 flex h-10 w-10 items-center justify-center rounded-full border border-green-900 bg-slate-950 text-xl text-green-400 [font-variant-numeric:tabular-nums]"
+      >
         {count}
-      </div>
+      </motion.div>
     </div>
   );
 };
