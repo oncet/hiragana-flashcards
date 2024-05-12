79d7cf46ca72d31d928676ed73db6c405915585d
oncet <camilorivera86@gmail.com>
2024-05-12 00:22:21 -0300

    new counter design

--- src/components/AcceptButton.tsx
+++ src/components/AcceptButton.tsx
@@ -1,19 +1,19 @@
-import { useState, useEffect, useRef } from "react";
 import { motion } from "framer-motion";
+import { useState } from "react";
 
 import Circle from "./Circle";
 
 const AcceptButton = () => {
   const [count, setCount] = useState(0);
-  const [animate, setAnimate] = useState(false);
+  // const [animate, setAnimate] = useState(false);
 
-  useEffect(() => {
-    if (animate) {
-      window.setTimeout(() => {
-        setAnimate(false);
-      }, 200);
-    }
-  }, [animate]);
+  // useEffect(() => {
+  //   if (animate) {
+  //     window.setTimeout(() => {
+  //       setAnimate(false);
+  //     }, 200);
+  //   }
+  // }, [animate]);
 
   return (
     <div className="relative">
@@ -21,15 +21,15 @@ const AcceptButton = () => {
         className="flex h-[110px] w-[110px] items-center justify-center rounded-full border-2 border-green-900"
         onClick={() => {
           setCount(count + 1);
-          setAnimate(true);
+          // setAnimate(true);
         }}
       >
         <Circle size="sm" />
       </button>
       <motion.div
-        animate={{ scale: animate ? 1.3 : 1 }}
+        // animate={{ scale: animate ? 1.3 : 1 }}
         transition={{ duration: 0.2 }}
-        className="absolute bottom-0 right-0 flex h-10 w-10 items-center justify-center rounded-full border border-green-900 bg-slate-950 text-xl text-green-400 [font-variant-numeric:tabular-nums]"
+        className="absolute bottom-0 right-0 flex h-10 w-10 items-center justify-center rounded-full border-2 border-green-900/50 bg-slate-950 text-lg font-semibold text-green-400/95 [font-variant-numeric:tabular-nums]"
       >
         {count}
       </motion.div>
