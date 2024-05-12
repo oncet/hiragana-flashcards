324016ce634fe73c7357e15d81078079d9a6235e
oncet <camilorivera86@gmail.com>
2024-05-12 00:15:32 -0300

    add pattern

--- src/App.tsx
+++ src/App.tsx
@@ -48,9 +48,12 @@ const App = () => {
         } p-[5mm]`}
       >
         <div
-          className={`flex h-full items-center justify-center rounded-[3.6mm] ${
-            isReverseVisible ? "" : "border border-solid border-slate-700"
-          } bg-slate-800 text-9xl font-bold text-slate-300`}
+          className={`flex h-full items-center justify-center rounded-[3.6mm] bg-slate-900 text-9xl font-bold text-slate-300`}
+          style={{
+            backgroundImage: isReverseVisible
+              ? ""
+              : `url("data:image/svg+xml,%3Csvg width='52' height='26' viewBox='0 0 52 26' xmlns='http://www.w3.org/2000/svg'%3E%3Cg fill='none' fill-rule='evenodd'%3E%3Cg fill='%23334155' fill-opacity='1'%3E%3Cpath d='M10 10c0-2.21-1.79-4-4-4-3.314 0-6-2.686-6-6h2c0 2.21 1.79 4 4 4 3.314 0 6 2.686 6 6 0 2.21 1.79 4 4 4 3.314 0 6 2.686 6 6 0 2.21 1.79 4 4 4v2c-3.314 0-6-2.686-6-6 0-2.21-1.79-4-4-4-3.314 0-6-2.686-6-6zm25.464-1.95l8.486 8.486-1.414 1.414-8.486-8.486 1.414-1.414z' /%3E%3C/g%3E%3C/g%3E%3C/svg%3E")`,
+          }}
         >
           {isReverseVisible ? (
             <div className="[transform:rotateY(180deg)]">A</div>
