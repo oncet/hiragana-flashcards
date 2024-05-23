e8d38e925a51b9ec49bc50101bd001d1b9a57864
oncet <camilorivera86@gmail.com>
2024-05-22 23:20:09 -0300

    add amplitude

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -5,5 +5,13 @@ export default defineConfig({
   plugins: [pluginReact()],
   html: {
     title: "Hirgana flashcards",
+    tags: [
+      {
+        tag: "script",
+        attrs: {
+          src: "https://cdn.amplitude.com/libs/analytics-browser-2.7.3-min.js.gz",
+        },
+      },
+    ],
   },
 });
