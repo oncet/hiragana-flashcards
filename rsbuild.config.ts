b72c5419e0f0ba46a35d42295c878400160985da
oncet <camilorivera86@gmail.com>
2024-05-22 23:26:09 -0300

    add missing scripts

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -12,6 +12,17 @@ export default defineConfig({
           src: "https://cdn.amplitude.com/libs/analytics-browser-2.7.3-min.js.gz",
         },
       },
+      {
+        tag: "script",
+        attrs: {
+          src: "https://cdn.amplitude.com/libs/plugin-autocapture-browser-0.9.0-min.js.gz",
+        },
+      },
+      {
+        tag: "script",
+        children:
+          "window.amplitude.add(window.amplitudeAutocapturePlugin.plugin());window.amplitude.init('77dbcaa6411540523a569b1d7b038bf9');",
+      },
     ],
   },
 });
